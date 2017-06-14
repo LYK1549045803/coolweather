6a63d86bd59609e3f30a877cb719948c801d5da9
Tony <tony@gmail.com>
2017-06-14 20:32:29 +0800

    加入显示天气信息的功能。

--- app/src/main/java/com/coolweather/app/util/Utility.java
+++ app/src/main/java/com/coolweather/app/util/Utility.java
@@ -1,6 +1,10 @@
 package com.coolweather.app.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
+import android.widget.SimpleAdapter;
 
 import com.coolweather.app.model.City;
 import com.coolweather.app.model.CoolWeatherDB;
@@ -9,10 +13,48 @@ import com.coolweather.app.model.Province;
 
 import junit.framework.Test;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by ibm on 2017/6/13.
  */
 public class Utility {
+
+    public static void handleWeatherResponse(Context context,String response){
+        try {
+            JSONObject jsonObject=new JSONObject(response);
+            JSONObject weatherInfo=jsonObject.getJSONObject("weatherInfo");
+            String cityName=weatherInfo.getString("city");
+            String weatherCode=weatherInfo.getString("cityId");
+            String temp1=weatherInfo.getString("temp1");
+            String temp2=weatherInfo.getString("temp2");
+            String weatherDesp=weatherInfo.getString("weather");
+            String publishTime=weatherInfo.getString("publishTime");
+            saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesp,publishTime);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+    public static void saveWeatherInfo(Context context,String cityName,String weatherCode,String temp1,String temp2,String weatherDesp,String publishTime){
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy年m月d日", Locale.CHINA);
+        SharedPreferences.Editor editor= PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected",true);
+        editor.putString("city_name",cityName);
+        editor.putString("weather_code",weatherCode);
+        editor.putString("temp1",temp1);
+        editor.putString("temp2",temp2);
+        editor.putString("weather_desp",weatherDesp);
+        editor.putString("publish_time",publishTime);
+        editor.putString("current_date",sdf.format(new Date()));
+        editor.commit();
+    }
+
+
     public synchronized static boolean handleProvincesResponse(CoolWeatherDB coolWeatherDB,String response){
         if (!TextUtils.isEmpty(response)){
             String[] allProvinces=response.split(",");
